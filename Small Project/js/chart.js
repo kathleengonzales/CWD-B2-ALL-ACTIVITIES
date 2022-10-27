b37fc165e5ae5fd195d9dcfc6c4e0e196fea1a41
kathleengonzales <116069251+kathleengonzales@users.noreply.github.com>
2022-10-27 21:35:10 +0800

    Add files via upload

---
+++ Small Project/js/chart.js
@@ -0,0 +1,30 @@
+window.onload = function () {
+ 
+    var chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        exportEnabled: true,
+        theme: "light1", // "light1", "light2", "dark1", "dark2"
+        title:{
+            text: "BLACKPINK Members Fans Chart"
+        },
+          axisY: {
+          includeZero: true
+        },
+        data: [{
+            type: "column", //change type to bar, line, area, pie, etc
+            //indexLabel: "{y}", //Shows y value on all Data Points
+            indexLabelFontColor: "#5A5757",
+              indexLabelFontSize: 16,
+            indexLabelPlacement: "outside",
+            dataPoints: [
+                { x: 10, y: 71 },
+                { x: 20, y: 55 },
+                { x: 30, y: 50 },
+                { x: 40, y: 65 },
+                
+            ]
+        }]
+    });
+    chart.render();
+     
+    }              
